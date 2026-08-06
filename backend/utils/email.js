f23b4c6a0e825677162ce5d57aecd05b3aa4d417
Gabryldev <fflorddrex@gmail.com>
2026-08-06 07:40:30 +0000

    Switch email service to Nodemailer

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -2,9 +2,7 @@ const fs = require("fs");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: false, // Use false for port 587
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
